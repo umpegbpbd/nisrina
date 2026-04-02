ea3fdbedea0876427e7d0757ba8b7610f43bda21
umpegbpbd <radityasukmana81@gmail.com>
2026-04-02 08:58:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = Object.freeze({
+  GOOGLE_CLIENT_ID: "PASTE_YOUR_GOOGLE_CLIENT_ID_HERE",
+  GOOGLE_SCOPE: "https://www.googleapis.com/auth/drive",
+  TARGET_FOLDER_ID: "1HTPk36dXLKin6xclSRQC2bAQ_KIFT-9H",
+  METADATA_FILE_NAME: "pns_berprestasi.json",
+  MAX_FILE_SIZE_MB: 5
+});
